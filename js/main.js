3ad70d9ea75a29dcfaa2b7fc1d5ca0626a3f58ca
cluelessengineer101 <112067252+cluelessengineer101@users.noreply.github.com>
2022-08-25 15:25:05 +0200

    put js and css in own files

---
+++ js/main.js
@@ -0,0 +1,188 @@
+var hunts=[]
+//a hunt consists of 30 elements. true=good (anything but mimic), false=mimic
+let hunt_1=[
+	[true,true,true,true,false,true,true,true,false,true],
+	[true,true,true,true,true,false,true,true,true,true],
+	[true,true,true,true,true,true,true,true,true,false]
+]
+hunts.push(hunt_1)
+//
+let hunt_2=[
+	[true,true,true,true,true,true,true,true,false,false],
+	[false,true,true,true,true,false,true,true,true,true],
+	[true,true,true,true,true,true,true,true,true,true]
+]
+hunts.push(hunt_2)
+//
+hunt_3=[
+	[true,true,true,true,true,true,false,true,true,true],
+	[true,true,true,true,false,true,true,true,true,true],
+	[true,true,true,true,false,true,true,true,true,true]
+]
+hunts.push(hunt_3)
+//
+hunt_4=[
+	[false,false,true,true,true,true,true,true,false,true],
+	[true,true,true,true,true,true,true,false,true,true],
+	[true,true,true,true,true,true,true,true,true,true]
+]
+hunts.push(hunt_4)
+//
+hunt_1=[
+[true,true,false,true,true,true,false,true,true,true,],
+[true,false,true,true,true,true,true,true,true,true],
+[true,true,true,true,true,false,true,true,true,true,]
+]
+hunts.push(hunt_1)
+//
+hunt_2=[
+[true,false,true,false,true,false,true,true,true,true],
+[true,false,true,true,true,true,true,true,true,true],
+[true,true,true,true,true,true,true,true,true,true]
+]
+hunts.push(hunt_2)
+//
+hunt_3=[
+[false,true,false,true,false,true,true,true,true,true],
+[true,true,true,true,true,true,true,true,true,true],
+[true,true,true,false,true,true,true,true,true,true]
+]
+hunts.push(hunt_3)
+//
+hunt_4=[
+[true,true,true,true,true,false,true,true,true,true],
+[true,true,true,false,true,true,true,true,true,true],
+[true,false,true,true,true,true,true,false,true,true]
+]
+hunts.push(hunt_4)
+//
+hunt_4=[
+[true,true,true,false,true,true,true,true,true,true],
+[true,true,true,false,true,true,true,true,true,true],
+[true,true,true,true,true,false,true,false,true,true]
+]
+hunts.push(hunt_4)
+//
+hunt_4=[
+[true,true,true,true,true,true,true,true,true,false],
+[true,false,true,true,true,true,true,true,false,true],
+[true,false,true,true,true,true,true,true,true,true]
+]
+hunts.push(hunt_4)
+//
+hunt_4=[
+[true,true,true,false,true,true,true,true,true,true],
+[true,true,true,true,true,true,true,false,true,true],
+[false,true,true,true,true,true,true,false,true,true]
+]
+hunts.push(hunt_4)
+//
+hunt_4=[
+[true,true,true,true,true,true,true,true,true,true],
+[true,true,false,true,true,true,true,true,true,true],
+[false,true,true,true,false,true,true,true,false,true]
+]
+hunts.push(hunt_4)
+//
+hunt_4=[
+[true,true,true,false,true,false,true,true,false,true],
+[true,true,true,true,true,false,true,true,true,true],
+[true,true,true,true,true,true,true,true,true,true]
+]
+hunts.push(hunt_4)
+hunt_4=[
+[true,true,false,true,true,true,true,true,true,true],
+[true,true,true,true,true,true,true,true,true,true],
+[true,true,true,false,true,true,false,true,false,true]
+]
+hunts.push(hunt_4)
+/*
+hunt_4=[
+[true,true,true,true,true,true,true,true,true,true],
+[true,true,true,true,true,true,true,true,true,true],
+[true,true,true,true,true,true,true,true,true,true]
+]
+hunts.push(hunt_4)
+*/
+
+function cleanup_hunts(){
+	let recurse=false;
+	let hl=hunts.length;
+	for(let j=0;j<hl;j++){
+		if(hunts[j]==null){
+			hunts.splice(j,1)
+			recurse=true;
+		}
+	}
+	if(recurse){
+		cleanup_hunts()
+	}
+}
+function handle_hunts(ds,is_chest){
+	for(let i=0;i<hunts.length;i++){
+		let curr_hunt=hunts[i]
+		if(curr_hunt[ds.i][ds.j]!=is_chest){
+			delete hunts[i]
+		}
+	}
+	cleanup_hunts()
+	if(hunts.length<1){
+		alert('no more hunts. pleas add data')
+	}
+}
+function print_hunts(){
+	let huntsdiv=document.getElementById('hunts')
+	huntsdiv.innerHTML=''
+	for(let hunt of hunts){
+		if(hunt.length>1){
+			let huntdiv=document.createElement('div')
+			huntdiv.classList.add('hunt')
+			for(let chestrow of hunt){
+				for(let chest of chestrow){
+					let chestdiv=document.createElement('div')
+					if(chest){
+						chestdiv.classList.add('good')
+					}else{
+						chestdiv.classList.add('bad')
+					}
+					huntdiv.appendChild(chestdiv)
+				}
+				
+			}
+			huntsdiv.appendChild(huntdiv)
+		}
+	}
+}
+
+function handleLeftClick(elem){
+	elem.classList.remove('unknown')
+	elem.classList.add('good')
+	let ds=elem.dataset
+	handle_hunts(ds,true)
+	print_hunts()
+}
+function handleRightClick(elem){
+	elem.classList.remove('unknown')
+	elem.classList.add('bad')
+	let ds=elem.dataset
+	handle_hunts(ds,false)
+	print_hunts()
+}
+window.onload=function(e){
+	let resdiv=document.getElementById('result')
+	for(i=0;i<3;i++){
+		for(j=0;j<10;j++){
+			let item= document.createElement('div');
+			item.classList.add("unknown");
+			item.dataset.i=i
+			item.dataset.j=j
+			item.onclick=function(){handleLeftClick(item)}
+			item.oncontextmenu=function(){handleRightClick(item)}
+			resdiv.appendChild(item)
+		}
+		let br=document.createElement('div')
+		br.classList.add("stopper");
+		resdiv.appendChild(br)
+	}
+	print_hunts();
+}
